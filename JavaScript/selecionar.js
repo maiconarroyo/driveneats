7e2670181f804af295950f9931867e421dabda84
Maicon Machado <147193871+maiconarroyo@users.noreply.github.com>
2024-10-17 08:57:24 -0300

    adicionado texto com nome das comidas e preços em checkout

--- JavaScript/selecionar.js
+++ JavaScript/selecionar.js
@@ -2,39 +2,45 @@ let comidaSelecionada = false;
 let bebidaSelecionada = false;
 let sobremesaSelecionada = false;
 
-function selecionarComida(comida) {
-    const botao = comida;
-const botaoSelecionadoAntes = document.querySelector('.main-food.selecionado'); 
+let comida = '';
+let bebida = '';
+let sobremesa = '';
+
+function selecionarComida(comidaElement) {
+    const botao = comidaElement;
+    const botaoSelecionadoAntes = document.querySelector('.main-food.selecionado'); 
     if (botaoSelecionadoAntes !== null) {
         botaoSelecionadoAntes.classList.remove('selecionado'); 
     }
     botao.classList.add('selecionado'); 
     comidaSelecionada = true;
+    comida = botao.querySelector('.sub-title.-foodName').textContent;
     verificarPedido();
 }
 
-function selecionarBebida(bebida) {
-    const botao = bebida
+function selecionarBebida(bebidaElement) {
+    const botao = bebidaElement;
     const botaoSelecionadoAntes = document.querySelector('.main-drink.selecionado'); 
     if (botaoSelecionadoAntes !== null) {
         botaoSelecionadoAntes.classList.remove('selecionado'); 
     }
     botao.classList.add('selecionado'); 
     bebidaSelecionada = true;
+    bebida = botao.querySelector('.sub-title.-foodName').textContent; 
     verificarPedido();
 }
 
-function selecionarSobremesa(sobremesa) {
-    const botao = sobremesa
+function selecionarSobremesa(sobremesaElement) {
+    const botao = sobremesaElement;
     const botaoSelecionadoAntes = document.querySelector('.main-candy.selecionado');
     if (botaoSelecionadoAntes !== null) {
         botaoSelecionadoAntes.classList.remove('selecionado'); 
     }
     botao.classList.add('selecionado');
     sobremesaSelecionada = true;
+    sobremesa = botao.querySelector('.sub-title.-foodName').textContent;
     verificarPedido();
 }
-
 
 function verificarPedido() {
     if (comidaSelecionada && bebidaSelecionada && sobremesaSelecionada) {
@@ -51,6 +57,8 @@ function fecharPedido() {
         document.querySelector('.overlay').style.display = 'flex';
         document.querySelector('.fechar-pedido').style.display = 'flex';
         document.querySelector('.sub-title.-buttom').classList.add('escondido');
+        const texto =  `<br><br>${comida}: 14.90 <br><br>${bebida}: 4.90 <br><br>${sobremesa}: 7.90`;
+        document.querySelector('.sub-title.-total').innerHTML = texto;
     }
 }
 
